d67f1bf0b0db5b4ff584a81d90d8f04b2e970748
Hajar el majid <hajarelmajid3@gmail.com>
2022-04-19 16:54:06 +0200

    firstCommitTvaProject

---
+++ TvaDeclarationApp/Menu.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TvaDeclarationApp
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.btnFournisseur = new System.Windows.Forms.Button();
+            this.btnSociete = new System.Windows.Forms.Button();
+            this.btnAssocie = new System.Windows.Forms.Button();
+            this.btnPeriod = new System.Windows.Forms.Button();
+            this.btnExercice = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelMenu.Controls.Add(this.button6);
+            this.panelMenu.Controls.Add(this.btnExercice);
+            this.panelMenu.Controls.Add(this.btnPeriod);
+            this.panelMenu.Controls.Add(this.btnAssocie);
+            this.panelMenu.Controls.Add(this.btnSociete);
+            this.panelMenu.Controls.Add(this.btnFournisseur);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 761);
+            this.panelMenu.TabIndex = 0;
+            this.panelMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMenu_Paint);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // btnFournisseur
+            // 
+            this.btnFournisseur.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnFournisseur.FlatAppearance.BorderSize = 0;
+            this.btnFournisseur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFournisseur.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnFournisseur.Image = ((System.Drawing.Image)(resources.GetObject("btnFournisseur.Image")));
+            this.btnFournisseur.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFournisseur.Location = new System.Drawing.Point(0, 80);
+            this.btnFournisseur.Name = "btnFournisseur";
+            this.btnFournisseur.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnFournisseur.Size = new System.Drawing.Size(220, 70);
+            this.btnFournisseur.TabIndex = 1;
+            this.btnFournisseur.Text = "Fournisseur";
+            this.btnFournisseur.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnFournisseur.UseVisualStyleBackColor = true;
+            this.btnFournisseur.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSociete
+            // 
+            this.btnSociete.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSociete.FlatAppearance.BorderSize = 0;
+            this.btnSociete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSociete.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnSociete.Image = ((System.Drawing.Image)(resources.GetObject("btnSociete.Image")));
+            this.btnSociete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSociete.Location = new System.Drawing.Point(0, 150);
+            this.btnSociete.Name = "btnSociete";
+            this.btnSociete.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnSociete.Size = new System.Drawing.Size(220, 70);
+            this.btnSociete.TabIndex = 2;
+            this.btnSociete.Text = "Société";
+            this.btnSociete.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSociete.UseVisualStyleBackColor = true;
+            // 
+            // btnAssocie
+            // 
+            this.btnAssocie.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAssocie.FlatAppearance.BorderSize = 0;
+            this.btnAssocie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAssocie.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnAssocie.Image = ((System.Drawing.Image)(resources.GetObject("btnAssocie.Image")));
+            this.btnAssocie.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAssocie.Location = new System.Drawing.Point(0, 220);
+            this.btnAssocie.Name = "btnAssocie";
+            this.btnAssocie.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnAssocie.Size = new System.Drawing.Size(220, 70);
+            this.btnAssocie.TabIndex = 3;
+            this.btnAssocie.Text = "Associé";
+            this.btnAssocie.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnAssocie.UseVisualStyleBackColor = true;
+            // 
+            // btnPeriod
+            // 
+            this.btnPeriod.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPeriod.FlatAppearance.BorderSize = 0;
+            this.btnPeriod.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPeriod.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnPeriod.Image = ((System.Drawing.Image)(resources.GetObject("btnPeriod.Image")));
+            this.btnPeriod.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPeriod.Location = new System.Drawing.Point(0, 290);
+            this.btnPeriod.Name = "btnPeriod";
+            this.btnPeriod.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnPeriod.Size = new System.Drawing.Size(220, 70);
+            this.btnPeriod.TabIndex = 5;
+            this.btnPeriod.Text = "Période";
+            this.btnPeriod.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPeriod.UseVisualStyleBackColor = true;
+            // 
+            // btnExercice
+            // 
+            this.btnExercice.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExercice.FlatAppearance.BorderSize = 0;
+            this.btnExercice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExercice.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnExercice.Image = ((System.Drawing.Image)(resources.GetObject("btnExercice.Image")));
+            this.btnExercice.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExercice.Location = new System.Drawing.Point(0, 360);
+            this.btnExercice.Name = "btnExercice";
+            this.btnExercice.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnExercice.Size = new System.Drawing.Size(220, 70);
+            this.btnExercice.TabIndex = 6;
+            this.btnExercice.Text = "    Exercice";
+            this.btnExercice.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExercice.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.Gainsboro;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(0, 430);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.button6.Size = new System.Drawing.Size(220, 70);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Déduction";
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1202, 761);
+            this.Controls.Add(this.panelMenu);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panelMenu;
+        private Button btnFournisseur;
+        private Panel panelLogo;
+        private Button button6;
+        private Button btnExercice;
+        private Button btnPeriod;
+        private Button btnAssocie;
+        private Button btnSociete;
+    }
+}
